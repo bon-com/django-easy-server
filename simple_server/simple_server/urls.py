72a1be86dda1195b40aaadcb60df000a85f1c96f
bon-com <iwatamasateru1111@gmail.com>
2023-07-23 05:49:56 +0900

    blackフォーマットツール追加、整形

--- simple_server/simple_server/urls.py
+++ simple_server/simple_server/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import index, index_json, index_html
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("simple/", index),
     path("simple/json/", index_json),
-    path("simple/html/", index_html)
+    path("simple/html/", index_html),
 ]
--- simple_server/simple_server/views.py
+++ simple_server/simple_server/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+
 def index(request):
-    """ 単純に文字列をレスポンスする """
+    """単純に文字列をレスポンスする"""
     return HttpResponse("Hello World")
 
+
 def index_json(request):
-    """ JSON文字列でレスポンスする """
+    """JSON文字列でレスポンスする"""
     data = dict(id=1, name="ヤマダ", mail="xxx@gmail.xxx", nums=[1, 2, 3, 4])
     # json_dumps_params={'ensure_ascii': False}とすることで、日本語のUnicodeエスケープを回避する
-    return JsonResponse(data, json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(data, json_dumps_params={"ensure_ascii": False})
+
 
 def index_html(request):
-    """ htmlでレスポンスする """
+    """htmlでレスポンスする"""
     ctx = {"name": "ヤマダ"}
     return render(request, "index.html", ctx)
