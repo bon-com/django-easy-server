628dd21545acb0f425b512eeda660f87d76db70c
bon-com <iwatamasateru1111@gmail.com>
2023-07-22 23:37:03 +0900

    初回コミット

---
+++ simple_server/simple_server/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from .views import index, index_json, index_html
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("simple/", index),
+    path("simple/json/", index_json),
+    path("simple/html/", index_html)
+]
---
+++ simple_server/simple_server/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+def index(request):
+    """ 単純に文字列をレスポンスする """
+    return HttpResponse("Hello World")
+
+def index_json(request):
+    """ JSON文字列でレスポンスする """
+    data = dict(id=1, name="ヤマダ", mail="xxx@gmail.xxx", nums=[1, 2, 3, 4])
+    # json_dumps_params={'ensure_ascii': False}とすることで、日本語のUnicodeエスケープを回避する
+    return JsonResponse(data, json_dumps_params={'ensure_ascii': False})
+
+def index_html(request):
+    """ htmlでレスポンスする """
+    ctx = {"name": "ヤマダ"}
+    return render(request, "index.html", ctx)
